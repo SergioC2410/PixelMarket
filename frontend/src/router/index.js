322657f88e2c1f3785e560b6246d564821937809
Sergio <sergiocontreraslopez607@gmail.com>
2025-03-20 01:16:54 -0400

    Añade nuevas rutas para registro, recuperación de contraseña y método de pago en el enrutador de Vue.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,8 +5,12 @@ import HomeView from '@/views/Home/HomeView.vue'; // Nueva vista
 import LoginView from '@/views/Auth/LoginView.vue';
 import ProductosView from '@/views/Productos/ProductosView.vue';
 import CheckoutView from '@/views/Checkout/CheckoutView.vue';
-import DetalleProducto from '@/views/Productos/DetalleProducto.vue';
-import CategoriasView from '@/views/Productos/CategoriasView.vue';
+import Register from '@/views/Auth/Register.vue';
+import Contraseña from '@/views/Auth/Contraseña.vue';
+import MetodoPago from '@/components/MetodoPago.vue';
+import DetalleProducto from '@/views/Productos/DetalleProducto.vue'; // Añadido
+import CategoriasView from '@/views/Productos/CategoriasView.vue'; // Añadido
+
 const routes = [
   { 
     path: '/', 
@@ -24,19 +28,34 @@ const routes = [
     component: ProductosView 
   },
   { 
-    path: '/productos/:id',
+    path: '/productos/:id', // Ruta dinámica para el detalle del producto
     name: 'DetalleProducto',
-    component: DetalleProducto
+    component: DetalleProducto 
   },
   {
-    path: '/categorias',
+    path: '/categorias', // Ruta para las categorías
     name: 'CategoriasView',
-    component: CategoriasView
+    component: CategoriasView 
   },
   { 
     path: '/checkout',
     name: 'Checkout',
     component: CheckoutView 
+  },
+  {
+    path: '/registro', 
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/Contraseña', 
+    name: 'Contraseña',
+    component: Contraseña
+  },
+  {
+    path: '/metodo-de-pago',
+    name: 'metodo-de-pago',
+    component: MetodoPago
   }
 ];
 
